a75da3e14e14a70eb7e57d1b780a56c23dd9c8c9
zhengjing <1056021017@qq.com>
2023-12-13 18:25:00 +0800

    标签页图标

--- src/components/tabbar/index.jsx
+++ src/components/tabbar/index.jsx
@@ -1,5 +1,21 @@
+import React, { useState, useCallback } from 'react'
+import homeIcon1 from '@/assets/homed.png'
+import homeIcon2 from '@/assets/home.png'
+import vipIcon1 from '@/assets/vipd.png'
+import vipIcon2 from '@/assets/vip.png'
+import mineIcon1 from '@/assets/mined.png'
+import mineIcon2 from '@/assets/mine.png'
 import './index.scss'
 const TabBar = () => {
-    return <div className="TabBar"></div>
+    const tabs = ['首页', '会员专区', '我的']
+    return (
+        <div className="TabBar">
+            {tabs.map((e) => (
+                <div className="TabBar_item" key={e}>
+                    <span></span>
+                </div>
+            ))}
+        </div>
+    )
 }
 export default TabBar
